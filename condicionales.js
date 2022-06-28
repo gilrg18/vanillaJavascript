26e1ee4594a6bafb886b2b50782cc3f6b0bfcfde
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-06-28 17:12:24 -0600

    condicionales

---
+++ condicionales.js
@@ -0,0 +1,17 @@
+if(10 > 15){
+    alert("Hola");
+}
+
+let nombre = "J";
+
+if(nombre == "Gil"){
+    alert(`hola ${nombre}`);
+}
+
+else if (nombre == "Gilberto"){
+    alert(`tu nombre es ${nombre}`);
+}
+
+else{
+    alert(`${nombre}`);
+}
