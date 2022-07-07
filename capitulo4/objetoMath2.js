269e1d1c6372069b7caddf8e34e5c7f7abbd0818
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-07-07 15:41:23 -0600

    objeto Math propiedades

---
+++ capitulo4/objetoMath2.js
@@ -0,0 +1,34 @@
+//Objeto Math propiedades
+//PI
+numero = Math.PI;
+document.write(numero + "<br>");
+
+//SQRT1_2 - raiz cuadrada de 1/2
+numero = Math.SQRT1_2;
+document.write(numero + "<br>");
+
+//SQRT2 - raiz cuadrada de 2
+numero = Math.SQRT2;
+document.write(numero + "<br>");
+
+
+//CONSTANTES Y LOGARITMOS
+//E -constante de euler, base de los logaritmos naturales, aprox 2.7182
+//LN2 - Log natural de 2, aprox .693
+//LN10 - Log natural de 10, aprox 2.303
+//LOG2E - Logaritmo de E con base 2, aprox 1.443
+//LOG10E - Logaritmo de E con base 10, aprox .434
+numero = Math.E;
+document.write(numero + "<br>");
+
+numero = Math.LN2;
+document.write(numero + "<br>");
+
+numero = Math.LN10;
+document.write(numero + "<br>");
+
+numero = Math.LOG2E;
+document.write(numero + "<br>");
+
+numero = Math.LOG10E;
+document.write(numero + "<br>");
