0a7e29743ed8a998dff7d0a3ad03bd3365034182
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-07-11 17:00:28 -0600

    Consola

---
+++ capitulo5/consola.js
@@ -0,0 +1,21 @@
+//FUNCIONES DE REGISTRO
+
+//assert() - aparece un mensaje de error en la consola si la afirmacion es falsa.
+//si la afirmacion es verdadera, no parecera nada. (NO ESTANDAR)
+
+//clear() - limpia la consola
+
+//error() - Muestra un mensaje de error en la consola
+
+//info() - Emite un mensaje informativo en la consola.
+//en firefox y chrome, se muestra un pequeno icono "i" junto a estos elemento en el registro de la consola
+
+//log() - muestra un mensaje en la consola ( o del interprete de javascript).
+
+//table() - toma un argumento obligatorio: data, debe ser un array u objeto, y un parametro adicional: columnos
+//y nos muestra una tabla en la consola
+
+//warn() - Imprime un mensaje de advertencia en la consola.
+
+//dir() - despliega una lista interactiva de las propiedades del objecto javascript
+//especificado. [NO ESTANDAR]
