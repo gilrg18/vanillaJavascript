1f552fc1ae1d01a204a8095f81d69a39d0f97811
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-07-18 14:17:21 -0600

    Metodos de atributos de un elemento - DOM

---
+++ capitulo6/atributosDOM.js
@@ -0,0 +1,16 @@
+//Metodos de atributos de un elemento
+//querySelector solo selecciona el primer elemento de la clase seleccionada
+const rangoEdad = document.querySelector(".rangoEdad");
+
+//document.write(rangoEdad); //[object HTMLInputElement]
+
+document.write("<br></br>"+rangoEdad.getAttribute("type"));//range
+//setAttribute("atributo a modificar", "nuevo valor")
+rangoEdad.setAttribute("type","text");
+document.write("<br></br>" + rangoEdad.getAttribute("type"));//color
+
+//No tiene valor, pero esta definido
+rangoEdad.setAttribute("type", "");
+
+//Quita el atributo type del elemento.
+rangoEdad.removeAttribute("type");
